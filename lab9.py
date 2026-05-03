d7de30370afc0f9958ee614b9bb7b326fd25c52c
Codovia <140329584+Codovia@users.noreply.github.com>
2026-05-03 13:42:49 +0530

    Remove unnecessary blank lines in lab9.py for cleaner code

--- lab9.py
+++ lab9.py
@@ -1,9 +1,7 @@
 import numpy as np
-
 from sklearn.datasets import fetch_olivetti_faces
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.naive_bayes import GaussianNB
-
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import matplotlib.pyplot as plt
 
@@ -15,7 +13,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 gnb = GaussianNB()
 gnb.fit(X_train, y_train)
-
 y_pred = gnb.predict(X_test)
 
 accuracy = accuracy_score(y_test, y_pred)
